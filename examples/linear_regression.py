b97ef275c4d213b0fe472022db1c97b46d2f7e3e
Johnson Zhang <Johnson9009@163.com>
2017-12-15 00:27:17 +0800

    Sync code.

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -38,13 +38,8 @@ def dataset_generator(dataset_size, dataset_per_file):
                 dataset_filenames.append(dataset_filename)
 
 
-def data_loader_iterator(shuffle=True, only_first=False):
+def data_loader_iterator(shuffle=True):
     ''' For this exercise we need load dataset from several CSV files. '''
-    if (only_first is True):
-        with open(dataset_filenames[0]) as f:
-            dataset = np.loadtxt(f, delimiter=',')
-            return (dataset[0, 0:1], dataset[0, 1:2])
-
     for dataset_filename in dataset_filenames:
         with open(dataset_filename) as f:
             dataset = np.loadtxt(f, delimiter=',')
--- python/dataset.py
+++ python/dataset.py
@@ -1,45 +1,59 @@
 import numpy as np
 
 
-def minibatch_iterator(data_loader_iterator, minibatch_size=64, shuffle=True):
+def minibatch_iterator(data_loader_iterator, sample_axis_at_end=False, minibatch_size=64, shuffle=True):
     ''' This is the mini batch generator which yield one mini batch dataset each time it is called,
     this generator is only responsible for shuffling the dataset given by data loader iterator and
     yielding one mini batch of it, when there isn't any data in this dataset data loader iterator
     will be called again until there isn't any more dataset from data loader. '''
+    sample_axis = -1 if (sample_axis_at_end is True) else 0
+
     for features, labels in data_loader_iterator(shuffle=shuffle):
-        samples_count = features.shape[0]
+        samples_count = features.shape[sample_axis]
         num_complete_minibatches = samples_count // minibatch_size
         sample_indexes = list(np.random.permutation(samples_count) if (shuffle is True) else range(samples_count))
 
         for i in range(num_complete_minibatches):
-            minibatch_features = features[sample_indexes[i * minibatch_size : (i + 1) * minibatch_size]]
-            minibatch_labels = labels[sample_indexes[i * minibatch_size : (i + 1) * minibatch_size]]
+            minibatch_features = np.take(features, sample_indexes[i * minibatch_size : (i + 1) * minibatch_size], axis=sample_axis)
+            minibatch_labels = np.take(labels, sample_indexes[i * minibatch_size : (i + 1) * minibatch_size], axis=sample_axis)
             yield (minibatch_features, minibatch_labels)
 
         if (samples_count % minibatch_size != 0):
-            minibatch_features = features[sample_indexes[num_complete_minibatches * minibatch_size : samples_count]]
-            minibatch_labels = labels[sample_indexes[num_complete_minibatches * minibatch_size : samples_count]]
+            minibatch_features = np.take(features, sample_indexes[num_complete_minibatches * minibatch_size : samples_count],
+                                         axis=sample_axis)
+            minibatch_labels = np.take(labels, sample_indexes[num_complete_minibatches * minibatch_size : samples_count],
+                                       axis=sample_axis)
             yield (minibatch_features, minibatch_labels)
 
 
-class StandardScarler(object):
+class StandardScaler(object):
     '''Standardize features by removing the mean and scaling to unit variance
 
     Centering and scaling happen independently on each feature by computing the relevant statistics on the samples in the training set.
     Mean and standard deviation are then stored to be used on later data using the transform method.
     '''
-    def __init__(self, data_loader_iterator, sample_axis=0, minibatch_size=64):
-        features_shape = list(data_loader_iterator(only_first=True)[0].shape)
+    def __init__(self, data_loader_iterator, sample_axis_at_end=False, minibatch_size=64):
+        minibatch_iter = minibatch_iterator(data_loader_iterator, sample_axis_at_end, minibatch_size, shuffle=False)
+        minibatch_features, _ = next(minibatch_iter)
+        features_shape = list(minibatch_features.shape)
+        sample_axis = -1 if (sample_axis_at_end is True) else 0
         features_shape[sample_axis] = 1
+
         self._mean = np.zeros(tuple(features_shape))
         self._stddev = np.ones(tuple(features_shape));
         self._samples_count = 0;
 
-        for minibatch_features, _ in minibatch_iterator(data_loader_iterator, minibatch_size, shuffle=False):
-            count = minibatch_features.shape[sample_axis]
-            self._samples_count += count;
-            self._mean += (count / self._samples_count) * (np.sum((1 / count) * minibatch_features, axis=sample_axis) - self._mean)
-            # TODO: Incremental iterative standard deviation computation.
+        while True:
+            try:
+                count = minibatch_features.shape[sample_axis]
+                self._samples_count += count;
+                self._mean += (count / self._samples_count) * (np.sum((1 / count) * minibatch_features,
+                                                                      axis=sample_axis, keepdims=True) - self._mean)
+                # TODO: Incremental iterative standard deviation computation.
+                minibatch_features, _ = next(minibatch_iter)
+            except StopIteration:
+                break
+
 
     def transform(self, features):
         '''Scaling features of X according to feature_range.'''
